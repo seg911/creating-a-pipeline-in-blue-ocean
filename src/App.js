3a11499ef26ebc33601e82cdb69ac4f1194ddca3
seg911 <40267111+seg911@users.noreply.github.com>
2020-02-06 23:58:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>HELLO WORLD!</code> and save to reload.
+          To get started, edit <code>BE BeBe GGG</code> and save to reload.
         </p>
       </div>
     );
